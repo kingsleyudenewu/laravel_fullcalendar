5beca983183dc386e4eff58ce4f5b0cad94fa50a
Kingsley Udenewu <kingzpacking@gmail.com>
2019-03-14 01:07:17 +0100

    Created a route for the calendar

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -10,7 +10,6 @@
 @extends('layout.main')
 
 @section('style')
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
 @endsection
 
@@ -22,6 +21,9 @@
                     <div class="card-header">
                         Laravel Full Calendar
                     </div>
+                    <div class="card-body">
+                        {!! $calendar->calendar() !!}
+                    </div>
                 </div>
             </div>
         </div>
@@ -29,7 +31,6 @@
 @endsection
 
 @section('script')
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
 @endsection
